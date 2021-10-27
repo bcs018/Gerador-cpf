4ef23386e51d883ce40ab4498681a228951af4bd
Bruno César <57082742+bcs018@users.noreply.github.com>
2021-10-27 11:49:46 -0300

    Adicionando layout

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador aleatório de CPFs</title>
+</head>
+<body>
+    <input type="number" name="qtd">
 
-<input type="number" name="qtd">
+    <button id="enviar">Enviar</button>
 
-<button id="enviar">Enviar</button>
+    <div id="message"></div>
+    <div id="loading"></div>
 
-<div id="message"></div>
-<div id="loading"></div>
-
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/script.js"></script> 
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script> 
+</body>
+</html>
